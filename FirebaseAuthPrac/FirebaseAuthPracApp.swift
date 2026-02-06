f962473542e0c15cee6227fc2dcb014a1df1f232
Rudraksh Chourey <Rudrakshchourey@outlook.com>
2026-02-06 10:00:45 -0800

    Initial Commit

---
+++ FirebaseAuthPrac/FirebaseAuthPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthPracApp.swift
+//  FirebaseAuthPrac
+//
+//  Created by rentamac on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
